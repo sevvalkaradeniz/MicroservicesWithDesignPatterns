4d5dbc657426cd76b6c4c4e1b5c26e47a7aaa958
sevvalkaradeniz <sevvalk@sabanciuniv.edu>
2023-07-18 15:31:26 +0300

    OrderCreatedEvent for Order.API microservice inside of shared class

---
+++ Shared/PaymentMessage.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared
+{
+    public class PaymentMessage
+    {
+        //One of the messages that will be send with OrderCreatedEvent
+        //should be public since other project will use them 
+        public string CardName { get; set; }
+
+        public string CardNumber { get; set; }
+
+        public string Expiration { get; set; }
+
+        public string CVV { get; set; }
+
+        public decimal TotalPrice { get; set; }
+    }
+}
