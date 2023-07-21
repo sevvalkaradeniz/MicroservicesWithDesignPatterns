45934a33a13c3b208224a4e979c9228d9f1482a1
sevvalkaradeniz <sevvalk@sabanciuniv.edu>
2023-07-21 14:42:45 +0300

    Stock model created, Seed created for in memory database

--- Payment.API/Program.cs
+++ Payment.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
@@ -22,4 +27,8 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+
+
+
+
+    app.Run();
---
+++ Stock.API/Model/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Stock.API.Model
+{
+    public class AppDbContext : DbContext //include base
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) // we will fill this opiton in the Program.cs file
+        {
+
+        }
+
+        public DbSet<Stock> Stocks { get; set; }
+    }
+}
