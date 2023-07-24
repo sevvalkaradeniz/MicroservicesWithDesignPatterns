b527879992c80f581c9a96ca63d2bc8e6b50a088
sevvalkaradeniz <sevvalk@sabanciuniv.edu>
2023-07-24 16:32:16 +0300

    StockAPI HTTPGet for remaining stock

---
+++ Stock.API/Controllers/StockController.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stock.API.Model;
+using System.Runtime.CompilerServices;
+
+namespace Stock.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        //get method
+        private readonly AppDbContext _context; // readonly used to set variables just in the constructor, we cannot set them somewhere else
+
+        public StockController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        
+        [HttpGet] //getting remainig stock from the database
+        public async Task<IActionResult> Get() {
+            return Ok(await _context.Stocks.ToListAsync());
+        }
+       
+    }
+}
